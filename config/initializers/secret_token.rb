7b30f45dcfc18670fc66608b6fdaf14d652ef4f4
Zafer CAKMAK <mzcakmak@gmail.com>
2012-05-22 23:12:43 +0300

    initial commit
    - added twitter bootstrap.
    - implemented devise and omniauth authentication.
    - facebook login works.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icravakti::Application.config.secret_token = '76f87adbf95880c7c101aec41ba41b7c8c48b861660d48e3aec664de30686a8b07a4eac90e2e10225bc8007733fd42321cace464401dad6503074b96b2d71a69'
